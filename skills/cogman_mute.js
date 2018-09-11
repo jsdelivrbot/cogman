f5ecd2ecc481ac38e09a238e339f3d48c57b1b31
Andreas Elia <andreaselia@live.co.uk>
2018-09-11 19:58:07 +0100

    admin checks which need testing

--- skills/cogman_mute.js
+++ skills/cogman_mute.js
@@ -1,61 +1,74 @@
 module.exports = function(controller) {
 
     controller.hears('mutees', 'direct_message,direct_mention,mention', function(bot, message) {
-        controller.storage.teams.get(bot.team_info.id, function (err, team) {
-            if (!team || !team.mutees || team.mutees.length == 0) {
-                respond(bot, message, 'No-one on your team is muted');
-            } else {
-                var mutees = team.mutees.map(mutee => `<@${mutee}>`).join(', ');
+        controller.storage.users.get(message.user, function (err, user) {
+            if (user && user.is_admin) {
+                controller.storage.teams.get(bot.team_info.id, function (err, team) {
+                    if (!team || !team.mutees || team.mutees.length == 0) {
+                        respond(bot, message, 'No-one on your team is muted');
+                    } else {
+                        var mutees = team.mutees.map(mutee => `<@${mutee}>`).join(', ');
 
-                respond(bot, message, 'Team mutees: ' + mutees);
+                        respond(bot, message, 'Team mutees: ' + mutees);
+                    }
+                });
             }
         });
+
     });
 
     controller.hears(['^mute (.*)'], 'direct_message,direct_mention,mention', function(bot, message) {
-        var userToMute = message.match[1].replace(/^\<\@/, '').replace(/\>$/, '');
+        controller.storage.users.get(message.user, function (err, user) {
+            if (user && user.is_admin) {
+                var userToMute = message.match[1].replace(/^\<\@/, '').replace(/\>$/, '');
 
-        controller.storage.teams.get(bot.team_info.id, function (err, team) {
-            if (team) {
-                if (!team.mutees) {
-                    team.mutees = [];
-                }
+                controller.storage.teams.get(bot.team_info.id, function (err, team) {
+                    if (team) {
+                        if (!team.mutees) {
+                            team.mutees = [];
+                        }
 
-                var userToUnmuteIndex = team.mutees.indexOf(userToMute);
+                        var userToUnmuteIndex = team.mutees.indexOf(userToMute);
 
-                if (userToUnmuteIndex !== -1) {
-                    respond(bot, message, '<@' + userToMute + '>' + ' is already muted');
-                } else {
-                    team.mutees.push(userToMute);
+                        if (userToUnmuteIndex !== -1) {
+                            respond(bot, message, '<@' + userToMute + '>' + ' is already muted');
+                        } else {
+                            team.mutees.push(userToMute);
 
-                    controller.storage.teams.save(team, function(err, saved) {
-                        respond(bot, message, '<@' + userToMute + '>' + ' has been muted');
-                    });
-                }
+                            controller.storage.teams.save(team, function(err, saved) {
+                                respond(bot, message, '<@' + userToMute + '>' + ' has been muted');
+                            });
+                        }
+                    }
+                });
             }
         });
     });
 
     controller.hears(['^unmute (.*)'], 'direct_message,direct_mention,mention', function(bot, message) {
-        var userToUnmute = message.match[1].replace(/^\<\@/, '').replace(/\>$/, '');
+        controller.storage.users.get(message.user, function (err, user) {
+            if (user && user.is_admin) {
+                var userToUnmute = message.match[1].replace(/^\<\@/, '').replace(/\>$/, '');
 
-        controller.storage.teams.get(bot.team_info.id, function (err, team) {
-            if (team) {
-                if (!team.mutees) {
-                    team.mutees = [];
-                }
+                controller.storage.teams.get(bot.team_info.id, function (err, team) {
+                    if (team) {
+                        if (!team.mutees) {
+                            team.mutees = [];
+                        }
 
-                var userToUnmuteIndex = team.mutees.indexOf(userToUnmute);
+                        var userToUnmuteIndex = team.mutees.indexOf(userToUnmute);
 
-                if (userToUnmuteIndex !== -1) {
-                    team.mutees.splice(userToUnmuteIndex, 1)
+                        if (userToUnmuteIndex !== -1) {
+                            team.mutees.splice(userToUnmuteIndex, 1)
 
-                    controller.storage.teams.save(team, function(err, saved) {
-                        respond(bot, message, '<@' + userToUnmute + '>' + ' has been unmuted');
-                    });
-                } else {
-                    respond(bot, message, '<@' + userToUnmute + '>' + ' is not in the mutees list');
-                }
+                            controller.storage.teams.save(team, function(err, saved) {
+                                respond(bot, message, '<@' + userToUnmute + '>' + ' has been unmuted');
+                            });
+                        } else {
+                            respond(bot, message, '<@' + userToUnmute + '>' + ' is not in the mutees list');
+                        }
+                    }
+                });
             }
         });
     });
--- skills/cogman_warn.js
+++ skills/cogman_warn.js
@@ -1,32 +1,40 @@
 module.exports = function(controller) {
 
     controller.hears('^warnings (.*)', 'direct_message,direct_mention', function(bot, message) {
-        var userToWarn = message.match[1].replace(/^\<\@/, '').replace(/\>$/, '');
+        controller.storage.users.get(message.user, function (err, user) {
+            if (user && user.is_admin) {
+                var userToWarn = message.match[1].replace(/^\<\@/, '').replace(/\>$/, '');
 
-        controller.storage.users.get(userToWarn, function (err, user) {
-            if (!user || !user.warnings || user.warnings.length == 0) {
-                bot.reply(message, 'User has no warnings');
-            } else {
-                var warnings = user.warnings.map(warnee => `${warnee}, `).join(', ');
+                controller.storage.users.get(userToWarn, function (err, user) {
+                    if (!user || !user.warnings || user.warnings.length == 0) {
+                        bot.reply(message, 'User has no warnings');
+                    } else {
+                        var warnings = user.warnings.map(warnee => `${warnee}, `).join(', ');
 
-                bot.reply(message, 'User warnings: ' + warnings);
+                        bot.reply(message, 'User warnings: ' + warnings);
+                    }
+
+                });
             }
-
         });
     });
 
     controller.hears('^clearWarnings (.*)', 'direct_message,direct_mention', function(bot, message) {
-        var userToClearWarningsFor = message.match[1].replace(/^\<\@/, '').replace(/\>$/, '');
+        controller.storage.users.get(message.user, function (err, user) {
+            if (user && user.is_admin) {
+                var userToClearWarningsFor = message.match[1].replace(/^\<\@/, '').replace(/\>$/, '');
 
-        controller.storage.users.get(userToClearWarningsFor, function (err, user) {
-            if (!user || !user.warnings || user.warnings.length == 0) {
-                bot.reply(message, 'User has no warnings');
-            } else {
-                user.warnings = [];
+                controller.storage.users.get(userToClearWarningsFor, function (err, user) {
+                    if (!user || !user.warnings || user.warnings.length == 0) {
+                        bot.reply(message, 'User has no warnings');
+                    } else {
+                        user.warnings = [];
 
-                controller.storage.users.save(user, function(err, saved) {
-                    if (saved) {
-                        bot.reply(message, 'User warnings cleared');
+                        controller.storage.users.save(user, function(err, saved) {
+                            if (saved) {
+                                bot.reply(message, 'User warnings cleared');
+                            }
+                        });
                     }
                 });
             }
@@ -34,36 +42,40 @@ module.exports = function(controller) {
     });
 
     controller.hears('^warn (.*)', 'direct_message,direct_mention', function(bot, message) {
-        var userToWarn = message.match[1].replace(/^\<\@/, '').replace(/\>$/, '');
+        controller.storage.users.get(message.user, function(err, user) {
+            if (user && user.is_admin) {
+                var userToWarn = message.match[1].replace(/^\<\@/, '').replace(/\>$/, '');
 
-        bot.startConversation(message, function(err, convo) {
-            convo.ask('What would you like to warn <@' + userToWarn + '> about?', function(response, convo) {
-                controller.storage.users.get(userToWarn, function (err, user) {
-                    if (user) {
-                        if (! user.warnings) {
-                            user.warnings = []
-                        }
+                bot.startConversation(message, function(err, convo) {
+                    convo.ask('What would you like to warn <@' + userToWarn + '> about?', function(response, convo) {
+                        controller.storage.users.get(userToWarn, function(err, user) {
+                            if (user) {
+                                if (! user.warnings) {
+                                    user.warnings = []
+                                }
 
-                        controller.storage.users.save(user, function(err, saved) {
-                            if (saved) {
-                                user.warnings.push(response.text);
+                                controller.storage.users.save(user, function(err, saved) {
+                                    if (saved) {
+                                        user.warnings.push(response.text);
 
-                                bot.api.im.open({user: userToWarn}, function(err, res) {
-                                    bot.sendEphemeral({
-                                        channel: res.channel.id,
-                                        user: userToWarn,
-                                        text: 'You have received a warning:\n' + response.text
-                                    });
+                                        bot.api.im.open({ user: userToWarn }, function(err, res) {
+                                            bot.sendEphemeral({
+                                                channel: res.channel.id,
+                                                user: userToWarn,
+                                                text: 'You have received a warning:\n' + response.text
+                                            });
+                                        });
+                                    }
                                 });
                             }
                         });
-                    }
+
+                        convo.next();
+
+                        convo.say('OK, I\'ve sent that warning.');
+                    });
                 });
-
-                convo.next();
-
-                convo.say('OK, I\'ve sent that warning.');
-            });
+            }
         });
     });
 
